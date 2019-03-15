37467f2d280863776611e7b70635b5077293f725
jolita3 <jolita3@inbox.lt>
2019-03-15 21:27:06 +0200

    test

--- list.php
+++ list.php
@@ -18,8 +18,6 @@
 
 
 
-
-
 		
 	<!-- Dropdown Structure -->
 <ul id="dropdown1" class="dropdown-content">
@@ -40,6 +38,111 @@
   </div>
 </nav>
         
+
+<div class="page-section bg-grey">
+		<div class="content-wrapper">
+
+			<ul class="gallery">
+				<li class="gallery-item">
+					<div class="image-wrapper">
+						<img src="images/marokas.jpg">
+						<div class="g-overlay"></div>
+					</div>
+				</li>
+				<li class="gallery-item">
+					<div class="image-wrapper">
+						<img src="images/jerusalem.jpg">
+						<div class="g-overlay"></div>
+					</div>
+				</li>
+				<li class="gallery-item">
+					<div class="image-wrapper">
+						<img src="images/madeira.jpg">
+						<div class="g-overlay"></div>
+					</div>
+				</li>
+				<li class="gallery-item">
+					<div class="image-wrapper">
+						<img src="images/lisbon.jpg">
+						<div class="g-overlay"></div>
+					</div>
+				</li>
+
+				<img class="gallery-arrow ga-left" src="images/left-arrow.png">
+				<img class="gallery-arrow ga-right" src="images/left-arrow.png">
+			</ul>
+
+			<div class="button-holder">
+				<a class="button" href=""><img class="btn-instagram" src="images/instagram.png">follow us on Instagram</a>
+			</div>
+		</div>
+	</div>
+
+<!-- 2 var -->
+ <div class="divider"></div>
+  <div class="section">
+  	<img src="images/marokas.jpg">
+    <h5>MOROCCO</h5>
+    <p>Stuff</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+  	<img src="images/jerusalem.jpg">
+    <h5>ISRAEL</h5>
+    <p>Stuff</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+  	<img src="images/madeira.jpg">
+    <h5>MADEIRA</h5>
+    <p>Stuff</p>
+  <div class="divider"></div>
+  <div class="section">
+  	<img src="images/lisbon.jpg">
+    <h5>PORTUGAL</h5>
+    <p>Stuff</p>  
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<div class="page-section bg-blue">
+		<div class="content-wrapper">
+			<h2 class="h-white">Contact Us</h2>
+
+			<div class="blocks-wrapper">
+				<div class="form-wrapper">
+					<form>
+						<input type="text" name="" placeholder="Full name">
+						<input type="text" name="" value="kontaktinis el. p.">
+						<textarea placeholder="Messege"></textarea>
+						<button>SEND</button>
+					</form>
+				</div>
+				<div class="map-wrapper">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849919!2d25.290128216029665!3d54.70991477940151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3+g.+5%2C+Vilnius+08240!5e0!3m2!1sen!2slt!4v1530566996484" width="100%" height="403" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
 
 
 
