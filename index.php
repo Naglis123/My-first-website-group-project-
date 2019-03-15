598fc0da621151ed6fe0ff9bc312e3d0e39d3e22
Silkunaite <joresilkunaite@gmail.com>
2019-03-15 21:00:59 +0200

    pirmas psl

--- index.php
+++ index.php
@@ -10,17 +10,24 @@
 </head>
 <body>
 
+
 	<div class="visasPsl" >
-		<div class="virsus" >
+		<div class="virsus">
+			<div class="shade"></div>
 
-			<img class="krioklys" src="images/pgr.jpg">
-
-
-
-
+			
+			<div class="title">
+				<h1>Welcome, let's travel the world!</h1>
+				<h5><span class="h-linija"></span>Since 2018<span class="h-linija"></span></h5>
+				<h4 class="about">About us</h3>
+			</div>
+			
 		</div>
 
+
+
 	</div>
+
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 	<script type="text/javascript" src="scripts/script.js"></script>
 
