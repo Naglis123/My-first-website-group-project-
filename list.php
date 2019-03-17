f5c5ad1267635eff9e2511802ba589e62afc6319
jolita3 <jolita3@inbox.lt>
2019-03-17 21:40:59 +0200

    list'o atnaujinimai - smulkus (sriftai sar)

--- list.php
+++ list.php
@@ -22,7 +22,7 @@
    <div class="section">
   	<img src="images/marokas.jpg">
     <h5>MOROCCO</h5>
-    <h6>Welcome to Morocco
+    <h6 class="h-white6">Welcome to Morocco
 Morocco is a gateway to Africa and a country of dizzying diversity. Here you'll find epic mountain ranges, ancient cities, sweeping deserts – and warm hospitality.
 Mountains & Desert
 From Saharan dunes to the peaks of the High Atlas, Morocco could have been tailor-made for travellers. Lyrical landscapes carpet this slice of North Africa like the richly coloured and patterned rugs you’ll lust after in local cooperatives. The mountains – not just the famous High Atlas but also the Rif and suntanned ranges leading to Saharan oases – offer simple, breathtaking pleasures: night skies glistening in the thin air, and views over a fluffy cloudbank from the Tizi n’Test pass. On lower ground, there are rugged coastlines, waterfalls and caves in forested hills, and the mighty desert.</h6>
@@ -37,7 +37,7 @@ From Saharan dunes to the peaks of the High Atlas, Morocco could have been tailo
     <div class="section">
   	<img src="images/jerusalem.jpg">
     <h5>ISRAEL</h5>
-    <h6>Welcome to Israel
+    <h6 class="h-white6">Welcome to Israel
 Few places on earth stir up passion the way that Israel does: the breathtaking beauty of its hills and valleys, the eerie stillness of the Dead Sea, the multi-coloured canyon of Makhtesh Ramon, and the ancient walls and pathways of Nazareth and Jerusalem. The call of the muezzin and the quiet prayers of Orthodox Jews at the Western Wall reflect how the religious devotion of the Muslims, Christians and Jews who live here runs through every facet of life.</h6>
     <button>More details...</button>
     </div>
@@ -50,7 +50,7 @@ Few places on earth stir up passion the way that Israel does: the breathtaking b
   <div class="section">
   	<img src="images/madeira.jpg">
     <h5>MADEIRA</h5>
-    <h6>Welcome to Madeira
+    <h6 class="h-white6">Welcome to Madeira
 Geologically dramatic, bursting with exotic colour and warmed year-round by the Atlantic sun, Portugal’s most enchanting island is a place that keeps all its subtropical holiday promises. Pearl of the Atlantic, island of eternal spring…Madeira well deserves its fanciful nicknames and the affection visitors and locals alike feel for this tiny volcanic island that offers so much.</h6>
     <button>More details...</button>
     </div>
