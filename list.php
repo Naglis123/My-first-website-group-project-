9939aa88f6246305db76969b3ee06003e2c64e67
jolita3 <jolita3@inbox.lt>
2019-03-17 20:40:02 +0200

    list'o atnaujinimai

--- list.php
+++ list.php
@@ -1,105 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Kelionių kryptys</title>
+	<title>Destinations</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-	
 </head>
 <body>
 
+<?php include "virsutineJuosta.php"; ?>
 
-<div>
+   
 
 
-
-
-		
-	<!-- Dropdown Structure -->
-<ul id="dropdown1" class="dropdown-content">
-  <li><a href="#!">one</a></li>
-  <li><a href="#!">two</a></li>
-  <li class="divider"></li>
-  <li><a href="#!">three</a></li>
-</ul>
-<nav>
-  <div class="nav-wrapper">
-    <a href="#!" class="brand-logo">Logo</a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <!-- Dropdown Trigger -->
-      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-    </ul>
-  </div>
-</nav>
-        
-
-<div class="page-section bg-grey">
-		<div class="content-wrapper">
-
-			<ul class="gallery">
-				<li class="gallery-item">
-					<div class="image-wrapper">
-						<img src="images/marokas.jpg">
-						<div class="g-overlay"></div>
-					</div>
-				</li>
-				<li class="gallery-item">
-					<div class="image-wrapper">
-						<img src="images/jerusalem.jpg">
-						<div class="g-overlay"></div>
-					</div>
-				</li>
-				<li class="gallery-item">
-					<div class="image-wrapper">
-						<img src="images/madeira.jpg">
-						<div class="g-overlay"></div>
-					</div>
-				</li>
-				<li class="gallery-item">
-					<div class="image-wrapper">
-						<img src="images/lisbon.jpg">
-						<div class="g-overlay"></div>
-					</div>
-				</li>
-
-				<img class="gallery-arrow ga-left" src="images/left-arrow.png">
-				<img class="gallery-arrow ga-right" src="images/left-arrow.png">
-			</ul>
-
-			<div class="button-holder">
-				<a class="button" href=""><img class="btn-instagram" src="images/instagram.png">follow us on Instagram</a>
-			</div>
-		</div>
-	</div>
 
 <!-- 2 var -->
  <div class="divider"></div>
-  <div class="section">
+ <div class="shade"></div>
+ <div class="fonas1">
+   <div class="section">
   	<img src="images/marokas.jpg">
     <h5>MOROCCO</h5>
+    <h6>Welcome to Morocco
+Morocco is a gateway to Africa and a country of dizzying diversity. Here you'll find epic mountain ranges, ancient cities, sweeping deserts – and warm hospitality.
+Mountains & Desert
+From Saharan dunes to the peaks of the High Atlas, Morocco could have been tailor-made for travellers. Lyrical landscapes carpet this slice of North Africa like the richly coloured and patterned rugs you’ll lust after in local cooperatives. The mountains – not just the famous High Atlas but also the Rif and suntanned ranges leading to Saharan oases – offer simple, breathtaking pleasures: night skies glistening in the thin air, and views over a fluffy cloudbank from the Tizi n’Test pass. On lower ground, there are rugged coastlines, waterfalls and caves in forested hills, and the mighty desert.</h6>
+    <button>More details...</button>
+    </div>
+    </div>
+    
     <p>Stuff</p>
-  </div>
+    
   <div class="divider"></div>
-  <div class="section">
+  <div class="fonas2">
+    <div class="section">
   	<img src="images/jerusalem.jpg">
     <h5>ISRAEL</h5>
-    <p>Stuff</p>
-  </div>
+    <h6>Welcome to Israel
+Few places on earth stir up passion the way that Israel does: the breathtaking beauty of its hills and valleys, the eerie stillness of the Dead Sea, the multi-coloured canyon of Makhtesh Ramon, and the ancient walls and pathways of Nazareth and Jerusalem. The call of the muezzin and the quiet prayers of Orthodox Jews at the Western Wall reflect how the religious devotion of the Muslims, Christians and Jews who live here runs through every facet of life.</h6>
+    <button>More details...</button>
+    </div>
+	</div>	
+    <p>Stuff</p>   
+  
   <div class="divider"></div>
+  <div class="fonas3">
   <div class="section">
   	<img src="images/madeira.jpg">
     <h5>MADEIRA</h5>
+    <h6>Welcome to Madeira
+Geologically dramatic, bursting with exotic colour and warmed year-round by the Atlantic sun, Portugal’s most enchanting island is a place that keeps all its subtropical holiday promises. Pearl of the Atlantic, island of eternal spring…Madeira well deserves its fanciful nicknames and the affection visitors and locals alike feel for this tiny volcanic island that offers so much.</h6>
+    <button>More details...</button>
+    </div>
+	</div>
     <p>Stuff</p>
+
   <div class="divider"></div>
+  <div class="fonas4">
   <div class="section">
   	<img src="images/lisbon.jpg">
     <h5>PORTUGAL</h5>
+    <h6>Welcome to Lisbon
+Seven cinematic hillsides overlooking the Rio Tejo cradle Lisbon's postcard-perfect panorama of cobbled alleyways, ancient ruins and white-domed cathedrals – a captivating scene crafted over centuries.</h6>
+    <button>More details...</button>
+    </div>
+	</div>
     <p>Stuff</p>  
   </div>
 
@@ -107,52 +74,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-	<div class="page-section bg-blue">
-		<div class="content-wrapper">
-			<h2 class="h-white">Contact Us</h2>
-
-			<div class="blocks-wrapper">
-				<div class="form-wrapper">
-					<form>
-						<input type="text" name="" placeholder="Full name">
-						<input type="text" name="" value="kontaktinis el. p.">
-						<textarea placeholder="Messege"></textarea>
-						<button>SEND</button>
-					</form>
-				</div>
-				<div class="map-wrapper">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849919!2d25.290128216029665!3d54.70991477940151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3+g.+5%2C+Vilnius+08240!5e0!3m2!1sen!2slt!4v1530566996484" width="100%" height="403" frameborder="0" style="border:0" allowfullscreen></iframe>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-
-
-
-
-
-
-
-
-
-	</div>
-
-
+<?php include "footeris.php"; ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 	<script type="text/javascript" src="scripts/script.js"></script>
+
 </body>
 </html>
